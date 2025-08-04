12f615b5afdbea754adba5f44e77032958b904d2
arrc10 <roshan50110@gmail.com>
2025-08-04 23:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ model = joblib.load('forecasting_ev_model.pkl')
 st.markdown("""
     <style>
         body {
-            background-color: #fcf7f7;
+            background-color: #FFFFFF;
             color: #000000;
         }
         .stApp {
